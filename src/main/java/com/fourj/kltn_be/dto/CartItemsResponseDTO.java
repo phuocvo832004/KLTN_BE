f2bfce028041822f12e6ac0c7800ba9cdc20cd20
phuocvo832004 <voroberrt@gmail.com>
2025-11-27 00:26:14 +0700

    cart

--- src/main/java/com/fourj/kltn_be/dto/CartItemsResponseDTO.java
+++ src/main/java/com/fourj/kltn_be/dto/CartItemsResponseDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Data
@@ -17,13 +18,22 @@ public class CartItemsResponseDTO {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Meta {
-        private int total;
+        private BigDecimal total;
     }
     
     public static CartItemsResponseDTO of(List<CartProductDTO> products) {
         CartItemsResponseDTO response = new CartItemsResponseDTO();
         response.setProducts(products);
-        response.setMeta(new Meta(products.size()));
+        
+        BigDecimal totalPrice = products.stream()
+                .map(p -> {
+                    BigDecimal price = p.getUnitPrice() != null ? p.getUnitPrice() : BigDecimal.ZERO;
+                    int qty = p.getQuantity() != null ? p.getQuantity() : 0;
+                    return price.multiply(BigDecimal.valueOf(qty));
+                })
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        
+        response.setMeta(new Meta(totalPrice));
         return response;
     }
 }
