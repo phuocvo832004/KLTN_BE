344b0f6e99e925032a0c31aaea46f4b48a919c78
phuocvo832004 <voroberrt@gmail.com>
2025-12-05 13:02:18 +0700

    cors

--- src/main/java/com/fourj/kltn_be/config/SecurityConfig.java
+++ src/main/java/com/fourj/kltn_be/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.fourj.kltn_be.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -30,6 +31,7 @@ public class SecurityConfig {
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/api/products/**").permitAll()
                 .requestMatchers("/api/reviews/product/**").permitAll()
